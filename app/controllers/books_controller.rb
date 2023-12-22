118627e2665e182178d92b6534a34833c54ef1ac
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2023-12-22 08:04:20 +0000

    add(10) 4-課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,18 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book)
+  end
+  
+  
+  
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
