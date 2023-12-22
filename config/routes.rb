709d591dd073316ecb97550b7031a703a6d48e5c
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2023-12-22 01:24:00 +0000

    add(6) 4-課題3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'homes/top'
   devise_for :users
   root to: 'homes#top'
+  
+  # resources :books
+  resources :books, only: [:new, :index, :show]
   resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
