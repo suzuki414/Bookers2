2cfdb9c4c1289e78a0ab15a555aee2a7f66a24e8
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2023-12-24 14:31:31 +0000

    add(20) 4-課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,24 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
     book = Book.new(book_params)
+    book.user_id = current_user.id
     book.save
-    redirect_to books_path
+    redirect_to book_path(book)
   end
 
   def index
     @books = Book.all
     @user = current_user
-    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = current_user
+    @books = Book.all
   end
 
   def edit
