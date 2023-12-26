69739a0c837f7975369e9a7fd70caa2b084a7a39
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2023-12-26 00:13:09 +0000

    add(30) 4-課題3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    # user = User.find(params[:id])
-    # unless user.id == current_user.id
-    #   redirect_to user_path(current_user)
-    # end
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
     
     @user = User.find(params[:id])
   end
